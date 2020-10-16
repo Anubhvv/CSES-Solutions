5210f1539679f407162ea4afbb01bb2061443284
Anubhav Agrawal <68989156+Anubhvv@users.noreply.github.com>
2020-10-16 13:03:15 +0530

    C++

--- Sorting and Searching/Movie Festival II.cpp
+++ Sorting and Searching/Movie Festival II.cpp
@@ -1,65 +1,38 @@
 #include <bits/stdc++.h>
+#include <array>
 #define loop(i,n) for(long long i=0;i<n;++i)
 using namespace std;
 int main()
 {
-    long long n,k;
-    long long a,d;
-
-    scanf("%lld %lld",&n,&k);
-    multiset <pair < long long ,long long > >arrival;
-    multiset < pair < long long, long long > >depart;
-    vector < pair < long long, long long > > sch;
-    //map<int, int > room;
-    multiset < pair < long long, long long > >::iterator itr;
+    int n,k;
+    int a,d;
+ 
+    scanf("%d %d",&n,&k);
+    multiset < int > depart;
+    array < int ,3 > dai[n];
+    multiset < int >::iterator itr;
     loop(i,n)
     {
-        scanf("%lld %lld",&a,&d);
-        sch.push_back(make_pair(a,d));
-        arrival.insert(make_pair(a,i));
-        depart.insert(make_pair(d,i));
+        scanf("%d %d",&a,&d);
+        dai[i][0]=d;
+        dai[i][1]=a;
     }
-    //int ans=0;
-    long long guest=0;
-    //int room_num=0;
-    long long ans=0;
-    while(arrival.size() and depart.size() and guest<k)
+    sort(dai,dai+n);
+    int ans=0;
+    loop(i,n)
     {
-       // ++room_num;
-       ++guest;
-        long long arr,ind,dept;
-        itr=depart.begin();
-        while(true)
+        itr=depart.lower_bound(dai[i][1]+1);
+        if(itr!=depart.begin())
         {
-            dept=(*itr).first;
-            ind=(*itr).second;
-            //cout<<" dept  = "<< dept << endl;
- //++ans;
+            --itr;
             depart.erase(itr);
+        }
+        if(depart.size()<k)
+        {
             ++ans;
-            //cout<<"room["<<ind<<"]="<<room_num<<endl;
-            //room[ind]=room_num;
-            //++ans;
-            arr=sch[ind].first;
-            //cout<<"arr = " << arr <<endl;
-
-            arrival.erase(arrival.find(make_pair(arr,ind)));
-            itr=arrival.lower_bound(make_pair( dept,0  ));
-            //itr=depart.lower_bound(make_pair(dept,10000000));
-            if(itr==arrival.end())
-                break;
-            ind=(*itr).second;
-            dept=sch[ind].second;
-            itr=depart.lower_bound(make_pair( dept,ind));
-            if(itr==depart.end())
-                break;
-           // ++ans;
-          //  ++itr;
-          //  cout<<" lower bound ("<<arr<<", "<<ind<<") = "<<(*itr).first<<' '<<(*itr).second<<endl;
+            depart.insert(dai[i][0]);
         }
     }
-    printf("%lld",ans);
-    //cout<<ans<<endl;
-
-    return 0;
-}
+    printf("%d",ans);
+ 
+    
