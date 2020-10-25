367163fd0f71db0df9fbfb0e3a80f5754a009c92
Anubhav Agrawal <68989156+Anubhvv@users.noreply.github.com>
2020-10-25 23:55:40 +0530

    C++ Bottom Up Approach
    0.65s
    knapsack

--- Dynamic Programming/Two Sets II.py
+++ Dynamic Programming/Two Sets II.py
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define loop(i,a,n) for(long long i=a;i<n;++i)
-int money[70000][501];
+int sumWays[70000][501];
 using namespace std;
 
 int main()
@@ -8,8 +8,7 @@ int main()
     int n;
     scanf("%d",&n);
     loop(i,0,n+1)
-        money[0][i]=1;
-    vector < int > sums;
+        sumWays[0][i]=1;
     int half_sum=(n*(n+1))/2;
     if(half_sum&1)
         cout<<0;
@@ -19,11 +18,11 @@ int main()
         loop(i,1,n+1)
         loop(j,1,half_sum+1)
         {
-            money[j][i]=money[j][i-1];
+            sumWays[j][i]=sumWays[j][i-1];
             if(j>=i)
-                money[j][i]=(money[j][i]+money[j-i][i-1])%1000000007;
+                sumWays[j][i]=(sumWays[j][i]+sumWays[j-i][i-1])%1000000007;
         }
-        printf("%d", money[half_sum][n-1] );
+        printf("%d", sumWays[half_sum][n-1] );
     }
     return 0;
 }
