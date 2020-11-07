239e9a2853a1f7a904f2c8ca98bea66e98243377
Anubhvv <68989156+Anubhvv@users.noreply.github.com>
2020-11-07 13:24:10 +0530

    C++ Modified Djikstra's SSSP algo

---
+++ Graph Algorithms/Flight Discount.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define loop(i, a, n) for(long long i = a; i < n; ++i)
+
+using namespace std;
+
+long long max(long long a, long long b)
+{
+    if(a<b)
+        return b;
+    return a;
+}
+int main()
+{
+    int n, m;
+    scanf("%d %d", & n, & m);
+    priority_queue< pair< long long, int > > maxheap;
+    vector < pair< int , long long > > adj[n+1];
+    loop(i,0,m)
+    {
+        int a,b;
+        long long w;
+        scanf("%d %d %lld",&a, &b, &w);
+        adj[a].push_back(make_pair(b,w));
+    }
+    long long distances[n+1];
+    long long distDiscount[n+1];
+    bool processed[n+1];
+    loop(i,0,n+1)
+    {
+        distances[i]=1000000000000000; //inf
+        distDiscount[i]=1000000000000000; //inf
+    }
+    distances[1]=0;
+    distDiscount[1]=0;
+    memset(processed,0,sizeof processed);
+    //DJIKSTRA's ALGO
+    maxheap.push(make_pair(0,1));
+    while(!maxheap.empty())
+    {
+        int cur_city = maxheap.top().second;
+        maxheap.pop();
+        if( ! processed[  cur_city ] )
+        {
+            processed[cur_city]=true;
+
+            loop( i , 0 , adj[cur_city].size() )
+            {
+                int city = adj[cur_city][i].first ;
+                long long dist=adj[cur_city][i].second;
+
+                if( distances[ city ] > distances[cur_city] + dist  ) //without discount (helpful in future discounts)
+                    distances[ city ] = distances[ cur_city ] + dist ;
+
+                if(distDiscount[ city ] > distDiscount[ cur_city ]+ dist) //discount already applied
+                    distDiscount[ city ] = distDiscount[ cur_city] + dist;
+
+                if(distDiscount[ city ] > distances[cur_city] + dist/2 ) //apply discount
+                    distDiscount[ city ] = distances[ cur_city ] + dist/2;
+
+                maxheap.push(make_pair( max( -distances[ city ]  , -distDiscount[ city ] ), city));
+            }
+        }
+    }
+    if(distances[n] > distDiscount[n])
+        cout<<distDiscount[n];
+    else cout<<distances[n];
+    return 0;
+}
